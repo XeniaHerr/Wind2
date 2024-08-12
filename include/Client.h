326777f8310545ef52a93ec083893b2f9cf9c47c
Xenia Herr <xeniaherr@gmail.com>
2024-08-12 12:43:24 +0200

    Added further documentation to the Client Class

--- include/Client.h
+++ include/Client.h
@@ -11,95 +11,145 @@
 
 namespace Wind {
 
-/**
- * @class 
- * @brief Modeling of a single window.
- *
- * A Window is a rectangle that belongs to an Application and can be managed more or less independently from other windows.
- * They are here called Clients. In a client the Window Id is stored alongside other metainformation about the window like
- * size and Positon.
- *
- * The main goal of this Class is to hold and manage these Metadata*/
-class Client {
+    /**
+     * @class 
+     * @brief Modeling of a single window.
+     *
+     * A Window is a rectangle that belongs to an Application and can be managed more or less independently from other windows.
+     * They are here called Clients. In a client the Window Id is stored alongside other metainformation about the window like
+     * size and Positon.
+     *
+     * The main goal of this Class is to hold and manage these Metadata*/
+    class Client {
 
-    public:
-
-
-        Client(Window win);
-
-        Client(const Client&& other);
-
-        ~Client() {
-            //Kill window
-        }
-
-        void sendevent();
-
-        void updategeom();
-
-        XWindowAttributes get_Window_Attributes();
+        public:
 
 
-        void setFullscreen();
+            /*
+             * @brief Constructor
+             *
+             * @param win X11 Window ID
+             *
+             * A Client shouldn't exist without a underlying Window
+             * */
+            Client(Window win);
 
-        void setDimensions(Dimensions dimensions);
+            /**
+             * @brief Move Constructor, nececcary for Holder Initializsation*/
+            Client(const Client&& other);
 
-        void setDimensions(u_int32_t x, u_int32_t y);
+            ~Client() {
+                //Kill window
+            }
 
+            void sendevent();
 
-        Topic& getOwner() const; 
-
-
-        Dimensions getCurrentDimensions() const; 
-
-        Dimensions getOldDimensions() const;
-
-        Position getPosition() const;
-
-        Position getOldPosition() const;
-        
-        void setPosition(Position pos);
-
-        Window getWindow() const;
-
-        void setOwner(Topic& t) ;
-
-        bool isOrphan() const;
-
-        bool isFloating() const;
-
-        void toggleFloating();
-
-        bool isVisible() const;
+            XWindowAttributes get_Window_Attributes();
 
 
-    private:
+            /**
+             * @brief Make Client fullscreen*/
+            void setFullscreen();
 
-        Window _window; /**< X11 Window ID */
+            /**
+             * @breif Set New Dimensions
+             * @param dimensions new Dimensions*/
+            void setDimensions(Dimensions dimensions);
 
-        Topic* _owner; /**< Currrent Topic*/
-
-
-        Dimensions currentDimension; /**< Current Dimensions(width, height)*/
-
-        Dimensions oldDimension; /**< Last Dimensions(width, height)*/
-
-
-        Position currentPosition; /**< Current Position(x,y)*/
-
-        Position oldPosition; /**< Last Position(x,y)*/
-
-
-        bool is_floating, /**< Floating Status*/ 
-             is_visible,  /**< Visibility status*/
-             is_fullscreen,  /**Fullscreen Status*/
-             is_orphan; /**< Has Owner?*/
+            /**
+             * @brief Set New Dimensions
+             *
+             * Alternative Overload takes raw Dimentions instead of a Dimensions
+             *
+             * @param x x Dimension
+             * @param y y Dimension*/
+            void setDimensions(u_int32_t x, u_int32_t y);
 
 
 
 
+            /**
+             * @brief set New position
+             *
+             * @param pos New Position*/
+            void setPosition(Position pos);
 
-};
+
+            void setOwner(Topic& t) ;
+
+
+
+            /**
+             * @brief toggle the Floating Status
+             *
+             * Toggles the is_floating variable. */
+            void toggleFloating();
+
+            /* Getter Function*/
+
+            /**
+             * @breif get the currentPosition
+             * @returns currentPosition*/
+            Position getPosition() const;
+
+            
+            /**
+             * @brief get the oldPosition
+             * @returns oldPosition*/
+            Position getOldPosition() const;
+
+
+            /**
+             * @brief get the current Owner
+             * @returns reference to _owner*/
+            Topic& getOwner() const; 
+
+            /**
+             * @brief get the currentDimension
+             * @returns currentDimension*/
+            Dimensions getCurrentDimensions() const; 
+
+            /**
+             * @brief get the last Dimensions
+             * @returns oldDimension*/
+            Dimensions getOldDimensions() const;
+
+            Window getWindow() const;
+
+            bool isOrphan() const;
+
+            bool isFloating() const;
+
+            bool isVisible() const;
+
+
+        private:
+
+            Window _window; /**< X11 Window ID */
+
+            Topic* _owner; /**< Currrent Topic*/
+
+
+            Dimensions currentDimension; /**< Current Dimensions(width, height)*/
+
+            Dimensions oldDimension; /**< Last Dimensions(width, height)*/
+
+
+            Position currentPosition; /**< Current Position(x,y)*/
+
+            Position oldPosition; /**< Last Position(x,y)*/
+
+
+            bool is_floating, /**< Floating Status*/ 
+                 is_visible,  /**< Visibility status*/
+                 is_fullscreen,  /**Fullscreen Status*/
+                 is_orphan; /**< Has Owner?*/
+
+
+
+
+
+    };
 
 
 }
